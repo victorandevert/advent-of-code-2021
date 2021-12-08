5f6dbfeb75a5febdf93b308d7803f37b12b3e8ed
victorandevert <victor.andevert@gmail.com>
2021-12-08 18:43:49 +0100

    add aim value for part two

--- src/main/kotlin/day2/Submarine.kt
+++ src/main/kotlin/day2/Submarine.kt
@@ -1,27 +1,41 @@
 package day2
 
+import kotlin.math.absoluteValue
+
 class Submarine {
     var forward = 0
     var depth = 0
+    var position = 0
+    var aim = 0
 
-    fun dive(command: String): Int{
-        val (direction, value) = Pair(
-            command.substringBefore(" ").trim(),
-            command.substringAfter(" ").trim().toInt())
 
-        if (direction == "forward"){
-            forward+=value
-        }else{
-            depth+=value
+    fun dive(command: List<String>): Int{
+        command.forEach {
+            val (direction, value) = Pair(
+                it.substringBefore(" ").trim(),
+                it.substringAfter(" ").trim().toInt()
+            )
+
+            if (direction == "forward") {
+                forward += value
+                if (aim >0){
+                    depth+=(value*aim)
+                }
+            } else if (direction == "up"){
+                aim-=value
+            }else if (direction=="down"){
+                aim+=value
+            }
+
         }
-
-        return if (forward!=0 && depth != 0) {
-            depth*forward
+        if (forward!=0 && depth != 0) {
+            position = depth*forward
         }else if (forward == 0) {
-            depth
+            position = aim
         }else{
-            forward
+            position = forward
         }
 
+        return position.absoluteValue
     }
 }
--- src/test/kotlin/day2/SubmarineShould.kt
+++ src/test/kotlin/day2/SubmarineShould.kt
@@ -3,6 +3,7 @@ package day2
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.io.File
 
 class SubmarineShould {
 
@@ -16,7 +17,7 @@ class SubmarineShould {
     @Test
     fun `get 5 as a final position after moving forward`() {
 
-        val result = submarine.dive("forward 5")
+        val result = submarine.dive(listOf("forward 5"))
 
         Assertions.assertThat(result).isEqualTo(5)
     }
@@ -24,9 +25,30 @@ class SubmarineShould {
     @Test
     fun `get 2 as a final position after moving downm`() {
 
-        val result = submarine.dive("down 2")
+        val result = submarine.dive(listOf("down 2"))
 
         Assertions.assertThat(result).isEqualTo(2)
     }
 
+    @Test
+    fun `move down`() {
+
+        val result = submarine.dive(listOf("down 5","up 3"))
+
+        Assertions.assertThat(result).isEqualTo(2)
+    }
+
+    @Test
+    fun `get 900 as a final position`() {
+
+        val result = submarine.dive(getCommands("submarine-commands.txt"))
+
+        Assertions.assertThat(result).isEqualTo(900)
+    }
+
+    
+   private fun getCommands(fileName: String): List<String> {
+        val uri = this.javaClass.classLoader.getResource(fileName)
+        return File(uri.file).readLines()
+    }
 }
