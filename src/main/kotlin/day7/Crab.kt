c051ee92562256b49a39d3fee99bf1c4f63b39bb
victorandevert <victor.andevert@gmail.com>
2021-12-10 20:40:15 +0100

    align crabs using the least amount of fuel

--- src/main/kotlin/day7/Crab.kt
+++ src/main/kotlin/day7/Crab.kt
@@ -1,7 +1,17 @@
 package day7
 
 class Crab {
-    fun alignHorizontally(positions: String): Long {
-        return 0
+    fun alignHorizontally(positions: List<Int>): Int = alignWithLessCost(1,positions, positions.sum())
+
+    private tailrec fun alignWithLessCost(n: Int, positions: List<Int>, fuelAmount: Int): Int{
+        if (n<=positions.maxOf { it }){
+            val newPositions=positions.map {
+                if(it>=n)it-n
+                else n-it
+            }
+            if(newPositions.sum()<fuelAmount) return alignWithLessCost(n+1, positions, newPositions.sum())
+        }
+        return fuelAmount
     }
 }
+
--- src/test/kotlin/day7/CrabShould.kt
+++ src/test/kotlin/day7/CrabShould.kt
@@ -3,6 +3,7 @@ package day7
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.io.File
 
 class CrabShould {
 
@@ -15,7 +16,22 @@ class CrabShould {
 
     @Test
     fun `align without wasting fuel`() {
-        assertThat(crab.alignHorizontally("5")).isEqualTo(0)
+        assertThat(crab.alignHorizontally(arrayListOf(5))).isEqualTo(0)
+    }
+
+    @Test
+    fun `align using the 1 fuel`() {
+        assertThat(crab.alignHorizontally(arrayListOf(16,15))).isEqualTo(1)
+    }
+
+    @Test
+    fun `align using the 37 fuel`() {
+        assertThat(crab.alignHorizontally(getCrabsFromFile("crabs.txt"))).isEqualTo(37)
+    }
+
+    private fun getCrabsFromFile(fileName: String): List<Int> {
+        val uri = this.javaClass.classLoader.getResource(fileName)
+        return File(uri.file).readText().split(",").map { it.toInt() }.toList()
     }
 
 }
